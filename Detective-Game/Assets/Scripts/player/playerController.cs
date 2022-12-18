b0251837ac63d97d536259df4b13fc563c756ff8
Furkan Alkılıç <fbfurkan2018@gmail.com>
2022-12-18 15:57:02 +0300

    update 0.0.1

---
+++ Detective-Game/Assets/Scripts/player/playerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private bool isGrounded;
+
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float jumpForce = 300f;
+    [SerializeField] private float gravityMod;
+
+    Rigidbody2D rbPlayer;
+
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody2D>();
+        Physics.gravity *= gravityMod;
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        //horizontalInput *= walkSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * walkSpeed);
+
+        checkIfPlayerJumped();
+    }
+
+    void OnCollisionEnter(Collision collider)
+    {
+        if(collider.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void checkIfPlayerJumped()
+    {
+        if(!isGrounded)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            isGrounded = false;
+            rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+    }
+}
